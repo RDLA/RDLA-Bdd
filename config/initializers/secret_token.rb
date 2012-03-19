f8a974fa6a366c07a00ba634dc74e510b394f589
Damien Brian <damien.brian@rdla.fr>
2012-03-19 13:12:23 +0100

    Initial commit, empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bdd::Application.config.secret_token = '9e94bce3f7cc6e502e9f03f47577441d6a70280c482477548c180e87d1f6184878126c60734de2645d62af165eaafbdd403bd93aa4080e0055472efc0a4d9506'
